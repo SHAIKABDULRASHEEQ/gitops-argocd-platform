d0f332f3e2396b14bedb2f5316696fc9da78ef31
EC2 Default User <ec2-user@ip-172-31-14-177.sa-east-1.compute.internal>
2026-05-10 11:12:36 +0000

    Version update

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "GitOps CI/CD Running"
+    return "GitOps version2  Running"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
